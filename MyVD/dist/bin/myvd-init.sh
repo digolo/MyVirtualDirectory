403ad799ecac9afc383c6f7e9d3279851be66fac
bigman921 <bigman921@9bc6254a-d524-0410-9dd1-bcce05b35c89>
2006-12-20 12:46:53 +0000

    Task-134304
    Created scripts for running MyVD on unix.  need to look for creating an executable for windows.

--- MyVD/dist/bin/myvd-init.sh
+++ MyVD/dist/bin/myvd-init.sh
@@ -13,7 +13,7 @@ done
 
 export CLASSPATH=$LOCAL_CP
 
-vldap="java -server org.sourceforge.myvd.server.Server $CONFIG_FILE"
+vldap="java -server net.sourceforge.myvd.server.Server $CONFIG_FILE"
 
 case "$1" in
 	"start")
