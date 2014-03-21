1327fbd9c53d38e50d9e9d21943ff2778b4648a9
bigman921 <bigman921@9bc6254a-d524-0410-9dd1-bcce05b35c89>
2014-03-21 13:15:25 +0000

    added an enabled option to the namespace configuration for quickly disabling namespaces without having to remove their configuration

--- trunk/MyVD/src/net/sourceforge/myvd/server/Server.java
+++ trunk/MyVD/src/net/sourceforge/myvd/server/Server.java
@@ -68,7 +68,7 @@ public class Server {
 	static Logger logger;
 	
 
-	public final static String VERSION = "0.9.1";
+	public final static String VERSION = "0.9.2";
 	
 	String configFile;
 	Properties props;
