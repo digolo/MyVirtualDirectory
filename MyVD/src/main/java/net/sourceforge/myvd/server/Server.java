ac8d6dd046fafa339abc2cc34c1e314cd69c3c6f
bigman921 <bigman921@9bc6254a-d524-0410-9dd1-bcce05b35c89>
2015-03-13 20:56:15 +0000

    added logging statements for ldap inerceptor

--- MyVD/src/main/java/net/sourceforge/myvd/server/Server.java
+++ MyVD/src/main/java/net/sourceforge/myvd/server/Server.java
@@ -68,7 +68,7 @@ public class Server {
 	static Logger logger;
 	
 
-	public final static String VERSION = "0.9.4.3";
+	public final static String VERSION = "0.9.4.4";
 	
 	String configFile;
 	Properties props;
--- MyVD/src/main/java/org/apache/directory/server/core/shared/DefaultCoreSession.java
+++ MyVD/src/main/java/org/apache/directory/server/core/shared/DefaultCoreSession.java
@@ -151,6 +151,7 @@ public class DefaultCoreSession implements CoreSession
         if (lsession != null) {
         	this.userSession = lsession;
         }
+        
     }
 
 
